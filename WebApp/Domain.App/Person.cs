917ea10d3f903a29aeaec12ad8c00f8fb4cb8b3e
krkatt <krkatt@ttu.ee>
2021-03-08 17:44:49 +0200

    Moving

---
+++ WebApp/Domain.App/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.App
+{
+    public class Person
+    {
+        
+        
+        [MaxLength(32)] public string FirstName { get; set; } = default!;
+        
+        [MaxLength(48)] public string LastName { get; set; } = default!;
+
+        public DateTime Date { get; set; } = default!;
+
+        public Char Sex { get; set; } = default!;
+        
+        
+    }
+}
---
+++ WebApp/Domain.App/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.App
+{
+    public class Role
+    {
+        [MaxLength(32)] public string Name { get; set; } = default!;
+
+        public DateTime Since { get; set; } = DateTime.Now;
+
+        public DateTime? Until { get; set; }
+
+        [MaxLength(128)] public string? Description { get; set; }
+    }
+}
